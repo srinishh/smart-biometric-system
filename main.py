3dcb4ba848c631bc3bf00c6a997a6808234248e5
srinishh <52791246+srinishh@users.noreply.github.com>
2020-12-14 14:28:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+from face_detection import *
+import pyttsx3
+
+engine = pyttsx3.init()
+
+engine.say("greetings can you please face the camera")
+
+
+
+engine.runAndWait()
+
+
+
+
+face_detection()
+
+
+
+
+
 
